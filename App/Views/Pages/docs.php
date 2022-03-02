c1b2817a363ff40866f42653dd814ee7ccc99197
Renan Salustiano <renan.silva621@etec.sp.gov.br>
2022-03-02 12:40:13 -0300

    Ajuste docs

--- App/Views/Pages/docs.php
+++ App/Views/Pages/docs.php
@@ -1,9 +1,3 @@
-<!-- HTML for static distribution bundle build -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Swagger UI</title>
     <link rel="stylesheet" type="text/css" href="<?php $this->publicUrl('/dist/swagger-ui.css')?>" />
     <link rel="icon" type="image/png" href="<?php $this->publicUrl('/dist/favicon-32x32.png')?>" sizes="32x32" />
     <link rel="icon" type="image/png" href="<?php $this->publicUrl('/dist/favicon-16x16.png')?>" sizes="16x16" />
@@ -28,9 +22,7 @@
             background: #fafafa;
         }
     </style>
-</head>
 
-<body>
 <div id="swagger-ui"></div>
 
 <script src="<?php $this->publicUrl('/dist/swagger-ui-bundle.js')?>" charset="UTF-8"> </script>
@@ -56,5 +48,3 @@
         window.ui = ui;
     };
 </script>
-</body>
-</html>
