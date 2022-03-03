271246e5ee5f6a9993c5ec21faae3cd3150e83c7
Renan Salustiano <renan.silva621@etec.sp.gov.br>
2022-03-03 03:45:42 -0300

    Comentários na classe Home

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -8,25 +8,38 @@ use App\Views\View;
 class Home{
 
 
+    /**
+     * Página inicial
+     */
     static function index(){
         $view = new View();
+        $vars = [];
+        $vars['title'] = 'Tela inciail';
         $view->setJsFile('salas.js');
         $view->setJsVar('const', 'listMethod', 'listRoom');
-        return $view->render('home');
+        return $view->render('home',$vars);
     }
 
-
+    /**
+     * Salas
+     */
     static function rooms(){
         $view = new View();
+        $vars = array();
+        $vars['title'] = 'Salas';
+
         $view->setJsFile('salas.js');
         $view->setJsVar('const', 'listMethod', 'listRoom');
-        return $view->render('rooms');
+        return $view->render('rooms',$vars);
     }
 
+    /**
+     * Agendamento
+     */
     static function scheduling(){
         $view = new View();
         $vars = [];
-
+        $vars['title'] = 'Agendamentos';
         $roomModel = new RoomModel();
         $rooms = $roomModel->getAll();
 
@@ -39,14 +52,22 @@ class Home{
         return $view->render('scheduling', $vars);
     }
 
+    /**
+     * Json swagger
+     */
     static function SwaggerJson(){
-        $openapi = \OpenApi\Generator::scan([SITE_ROOT."/App/Controllers/Api"]);
+        $openapi = \OpenApi\Generator::scan([SITE_ROOT."/App/Controllers/Api",SITE_ROOT."/App/Entity"]);
         header('Content-Type: application/json');
         echo $openapi->toJson();
     }
 
+    /**
+     * Documentação api
+     */
     static function docs(){
         $view = new View();
-        return $view->render('docs');
+        $vars = array();
+        $vars['title'] = 'Documentação da api';
+        return $view->render('docs',$vars);
     }
 }
